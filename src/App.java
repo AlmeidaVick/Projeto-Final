9a2becf281b1d72c77c7a2d39baa8fc5880985b8
Victoria Almeida <victoriaregia828@gmail.com>
2023-11-29 22:33:34 -0300

    Última modificação

--- src/App.java
+++ src/App.java
@@ -1,14 +1,20 @@
 public class App {
 
-       // Método main para teste
     public static void main(String[] args) {
+        
         // Criando um objeto Celular
         Celular meuCelular = new Celular("Galaxy S21", "Samsung", "123456789");
 
         // Imprimindo informações do celular
-        System.out.println("Informações do Celular:");
+        System.out.println("Informações iniciais do Celular:");
+        System.out.println();
         System.out.println(meuCelular);
-    }
+
 
         
-}
+
+
+        
+    }
+        }
+    
